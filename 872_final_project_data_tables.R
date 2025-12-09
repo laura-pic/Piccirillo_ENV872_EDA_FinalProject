d8388f841e4154a3d81c555fc5223fae2ff5c14a
laura-pic <laura.piccirillo@duke.edu>
2025-12-09 18:00:05 -0500

    new script file with backup markdown version of dataset tables

---
+++ 872_final_project_data_tables.R
@@ -0,0 +1,22 @@
+Table: Selenium Dataset Structure {#tab:table1}
+  
+  | Column                | Description                                                                      |
+    |:------------------|:----------------------------------------------------|
+    | EVENT_LAT             | Latitude at the start of the event, according to the event log (decimal degrees) |
+    | EVENT_LON             | Longitude according to the event log; West is negative (decimal degrees)         |
+    | DEPTH                 | Depth of sample (meters)                                                         |
+    | PI_DATE               | PI-supplied date in format YYYYMMDD                                              |
+    | Se_IV_D_CONC_BOTTLE   | Dissolved selenite concentration from a bottle sample (nanomoles/liter)          |
+    | Se_VI_D_CONC_BOTTLE   | Dissolved selenate concentration from a bottle sample (nanomoles/liter)          |
+    | Org_Se_II_CONC_BOTTLE | Organic selenide concentration from a bottle sample (nanomoles/liter)            |
+    
+    
+    ```
+  
+  | Column    | Description                                           |
+    |:----------|:------------------------------------------------------|
+    | DATE      | Station Date (GMT) YYYYMMDD                           |
+    | LATITUDE  | Station Latitude (South is negative; decimal degrees) |
+    | LONGITUDE | Station Longitude (West is negative; decimal degrees) |
+    | DepSM     | Depth salt water lat = -12.0057 (meters)              |
+      | Sbeox0    | Oxygen SBE 43 WS = 2 (umol/kg)                        |
